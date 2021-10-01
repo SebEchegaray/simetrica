e156c26c0084e034cd121ce66b4e0ecc32da753b
Seba <dj.sebamatzzo@gmail.com>
2021-10-01 20:24:42 +1000

    Fonts and Top main nav bar finished. Not responsive yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Home } from './components/Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom'
+import './components/Main.scss'
+import roundLogoBlue from './images/Round_Logo_Blue.png'
+import roundLogoWhite from './images/Round_Logo_White.png'
+import logoWhite from './images/Pentagon_Logo_White.png'
+import './fonts/MyriadPro-Regular.otf'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <nav className="header__nav">
+        <ul className="header__nav-ul">
+            <Link className="header__nav-li" to="/">Home</Link>
+            <Link className="header__nav-li" to="/galeria">Galeria</Link>
+        </ul>
+        <div className="rotating__logo-container">
+          <a href="/"><img src={logoWhite} alt="" className="rotating__logo" /></a>
+        </div>
+        <ul className="header__nav-ul">
+            <Link className="header__nav-li" to="/presupuestos">Presupuestos</Link>
+            <Link className="header__nav-li" to="/nosotros">Nosotros</Link>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Main.scss'
+
+export class Home extends Component  {
+  render() {
+    return (
+      <div className="content__main-wrapper">
+        <h1>Hellow HOME PAGE!</h1>
+      </div>
+    )
+  }
+}
